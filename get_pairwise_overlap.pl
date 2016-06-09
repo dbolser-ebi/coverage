3383c150c726cb1f0101ba1d6520679690b845ee
Dan Bolser <dbolser@ebi.ac.uk>
2016-06-09 13:01:28 +0100

    refactoring variable names

--- get_pairwise_overlap.pl
+++ get_pairwise_overlap.pl
@@ -8,33 +8,30 @@ use Set::IntRange;
 die "pass two range-files\n"
     unless @ARGV == 2;
 
-my $rf1 = $ARGV[0];
-my $rf2 = $ARGV[1];
+my ($range_file_1,
+    $range_file_2) = @ARGV;
 
-
-## This 'first pass' is needed for efficieny below. Basically the 'set
-## resize' operations are relatively costly, so by finding the maximum
-## set size required per ID up front, we save a lot of work later...
+## Parsing both files to get max range size (for efficiency)
 warn "Parsing pass 1/2\n";
 
-my %length;
+my %max_range_size;
 
-for my $range ($rf1, $rf2){
-    warn "$range\n";
+for my $range_file ($range_file_1, $range_file_2){
+    warn "$range_file\n";
     
-    open( my $RANGE, '<', $range )
-        or die "Failed to open '$range': $!\n";
+    open( my $RANGE, '<', $range_file )
+        or die "Failed to open '$range_file': $!\n";
     
     while(<$RANGE>){
         chomp;
         
-        my ($id, $st, $en) = split "\t";
+        my ($id, $start, $end) = split "\t";
         
-        $length{$id} = $en
-            if $en > ($length{$id} || 0)
+        $max_range_size{$id} = $end
+            if $end > ($max_range_size{$id} || 0)
     }
     
-    warn scalar keys %length, "\n";
+    warn scalar keys %max_range_size, "\n";
 }
 
 
@@ -44,11 +41,11 @@ warn "Parsing pass 2/2\n";
 my %range_sets;
 my %range_space;
 
-for my $range ($rf1, $rf2){
-    warn "$range\n";
+for my $range_file ($range_file_1, $range_file_2){
+    warn "$range_file\n";
     
-    open( my $RANGE, '<', $range )
-        or die "Failed to open '$range': $!\n";
+    open( my $RANGE, '<', $range_file )
+        or die "Failed to open '$range_file': $!\n";
     
     my $ranges = 0;
     my $range_space = 0;
@@ -56,28 +53,28 @@ for my $range ($rf1, $rf2){
     while(<$RANGE>){
         chomp;
         
-        my ($id, $st, $en) = split "\t";
+        my ($id, $start, $end) = split "\t";
         
         $ranges++;
-        $range_space += $en-$st+1;
+        $range_space += $end-$start+1;
     
-        $range_sets{$range}{$id} = Set::IntRange->new(1, $length{$id})
-            unless exists $range_sets{$range}{$id};
-        $range_sets{$range}{$id}->Interval_Fill($st, $en);
+        $range_sets{$range_file}{$id} = Set::IntRange->new(1, $max_range_size{$id})
+            unless exists $range_sets{$range_file}{$id};
+        $range_sets{$range_file}{$id}->Interval_Fill($start, $end);
     }
     
     warn "loaded $ranges ranges on ",
-    scalar keys %{$range_sets{$range}}, " seq-regions\n";
+    scalar keys %{$range_sets{$range_file}}, " seq-regions\n";
     
     my $range_space_no_overlap;
     $range_space_no_overlap += $_->Norm
-        for values %{$range_sets{$range}};
+        for values %{$range_sets{$range_file}};
     
     warn "range space is $range_space total ".
         "($range_space_no_overlap no-overlap) bp\n";
     
     ## Hide this value away...
-    $range_space{$range} = $range_space_no_overlap;
+    $range_space{$range_file} = $range_space_no_overlap;
 }
 
 
@@ -87,15 +84,15 @@ for my $range ($rf1, $rf2){
 my $coverage_sr = 0;
 my $coverage_bp = 0;
 
-for my $id (keys %{$range_sets{$rf1}}){
-    next unless exists $range_sets{$rf2}{$id};
+for my $id (keys %{$range_sets{$range_file_1}}){
+    next unless exists $range_sets{$range_file_2}{$id};
     
     $coverage_sr++;
     
-    my $intersection = Set::IntRange->new(1, $length{$id});
+    my $intersection = Set::IntRange->new(1, $max_range_size{$id});
     
-    $intersection->Intersection($range_sets{$rf1}{$id},
-                                $range_sets{$rf2}{$id});
+    $intersection->Intersection($range_sets{$range_file_1}{$id},
+                                $range_sets{$range_file_2}{$id});
     
     $coverage_bp += $intersection->Norm;
 }
@@ -103,25 +100,25 @@ for my $id (keys %{$range_sets{$rf1}}){
 warn "coverage of xxxxx ranges on $coverage_sr seq-regions\n";
 warn "coverage (non-overlapping overlap) is ", $coverage_bp, " bp\n";
 
-warn $coverage_bp / $range_space{$rf1} * 100,
+warn $coverage_bp / $range_space{$range_file_1} * 100,
     " of non-overlapping range space\n";
-warn $coverage_bp / $range_space{$rf2} * 100,
+warn $coverage_bp / $range_space{$range_file_2} * 100,
     " of non-overlapping range space\n";
 
 print
     join("\t",
-         (scalar keys %{$range_sets{$rf1}}),
-         $range_space{$rf1},
+         (scalar keys %{$range_sets{$range_file_1}}),
+         $range_space{$range_file_1},
          
-         (scalar keys %{$range_sets{$rf2}}),
-         $range_space{$rf2},
+         (scalar keys %{$range_sets{$range_file_2}}),
+         $range_space{$range_file_2},
 
          $coverage_sr,
          $coverage_bp,
          
-         $coverage_bp / $range_space{$rf1} * 100,
-         $coverage_bp / $range_space{$rf2} * 100,
+         $coverage_bp / $range_space{$range_file_1} * 100,
+         $coverage_bp / $range_space{$range_file_2} * 100,
          
-         $rf1,
-         $rf2,
+         $range_file_1,
+         $range_file_2,
     ), "\n";
